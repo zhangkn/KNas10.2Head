6045ab5a6ed1046b0cff39a4bd41450a818fae45
张坤楠 <zhangkunnan@qinbaowan.com>
2018-01-25 11:23:19 +0800

    Initial Commit

---
+++ as10.2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  as10.2
+//
+//  Created by devzkn on 25/01/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ as10.2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  as10.2
+//
+//  Created by devzkn on 25/01/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
